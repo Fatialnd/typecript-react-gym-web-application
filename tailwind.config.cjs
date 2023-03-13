6df5f1783e3b4916ce9ffa6c72f2f69e4ed04f7b
Fatima <75587225+Fatialnd@users.noreply.github.com>
2023-03-13 11:25:00 +0100

    Navbar menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,8 @@ module.exports = {
         "gray-100": "#DFCCCC",
         "gray-500": "#5E0000",
         "primary-100": "#FFE1E0",
-        "primary-300": "FFA6A3",
-        "primary-500": "FF6B66",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
         "secondary-400": "#FFCD58",
         "secondary-500": "#FFC132"
       },
@@ -30,7 +30,7 @@ module.exports = {
         abstractWaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
         circles: "url('./assets/Circles.png')",
-      }
+      },
     },
     screens: {
       xs: "480px",
