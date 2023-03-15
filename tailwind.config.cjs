1db96363c25a71de05b410636e3661cf3b0899e4
Fatima <75587225+Fatialnd@users.noreply.github.com>
2023-03-15 09:42:33 +0100

    navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -14,16 +11,17 @@ module.exports = {
         "primary-100": "#FFE1E0",
         "primary-300": "#FFA6A3",
         "primary-500": "#FF6B66",
-        "secondary-400": "#FFCD58",
-        "secondary-500": "#FFC132"
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
       },
       backgroundImage: (theme) => ({
-        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-        "mobile-home": "url('./assets/HomePageGraphic.png')"
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
-        montserrat: ["Montserrat", "sans-serif"]
+        montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
         evolvetext: "url('./assets/EvolveText.png')",
@@ -36,7 +34,7 @@ module.exports = {
       xs: "480px",
       sm: "768px",
       md: "1060px",
-    }
+    },
   },
   plugins: [],
-}
+};
