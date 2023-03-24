3bd7b58edacc1554f5ab56037cf9166dd5f2e0a9
Fatima <75587225+Fatialnd@users.noreply.github.com>
2023-03-24 14:29:34 +0100

    benefits page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
       },
       content: {
         evolvetext: "url('./assets/EvolveText.png')",
-        abstractWaves: "url('./assets/AbstractWaves.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
         circles: "url('./assets/Circles.png')",
       },
